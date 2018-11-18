cfc2cdc6fac90fd7f2be0882ef98b10503ccfdeb
luxaflow <34321224+luxaflow@users.noreply.github.com>
2018-11-18 22:48:33 +0100

    did inital setup for posts app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from accounts.models import Profile
 
-# Create your views here.
+
+def post_list(request): 
+    posts = Post.objects.all().order_by('-created_at')
+    comments = Comment.objects.all().order_by('-created_at')
+    print(posts)
+    context = {
+        'posts': posts,
+        'comments': comments
+    }
+    if request.user != None:
+        context.__setitem__('profile', Profile.objects.all().filter(user=request.user.id))
+    return render(request, 'posts/post_list.html', context)
+
+
+def post_create(request):
+    return render(request, 'posts/post_form.html')
